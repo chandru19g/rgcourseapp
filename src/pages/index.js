0575dbe63a4a09cc502c5451cceb34db088bcd2f
chandru19g <chandru1918g@gmail.com>
2021-05-28 22:37:39 +0530

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,72 @@
 import React from "react"
-import { graphql} from 'gatsby'
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/reuseable/HeroSection"
 import Infoblock from "../components/reuseable/Infoblock"
-import DualInfoBlock from "../components/reuseable/DualInfoBlock";
+import DualInfoBlock from "../components/reuseable/DualInfoBlock"
 import Coursecart from "../components/Cart/Coursecart"
 import Bundlecart from "../components/Cart/Bundlecart"
 
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <HeroSection 
-      img = {data.img.childImageSharp.fluid}
-      title = "I write code"
-      subtitle = "LearnCodeOnline.in"
-      heroclass ="hero-background"
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="I write code"
+      subtitle=""
+      heroclass="hero-background"
     />
     <Infoblock heading="About Us" />
     <Coursecart courses={data.courses} />
     <DualInfoBlock heading="Our Team" />
     <Bundlecart bundles={data.bundle} />
-
   </Layout>
 )
 
-export const query = graphql `
-{
-  img: file(relativePath: { eq: "heromain.png"}) {
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid_tracedSVG
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
       }
     }
-  }
-  courses: allContentfulCourses{
-    edges{
-      node{
-        id
-        title
-        price
-        description{
-          description
+    courses: allContentfulCourses {
+      edges {
+        node {
+          id
+          title
+          price
+          description {
+            description
+          }
+          category
+          image {
+            fixed(width: 200, height: 120) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
-        category
-        image{
-          fixed(width:200, height:120){
-            ...GatsbyContentfulFixed_tracedSVG
+      }
+    }
+    bundle: allContentfulBundle {
+      edges {
+        node {
+          id
+          title
+          price
+          image {
+            fixed(width: 200, height: 120) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
           }
         }
       }
     }
   }
-  bundle: allContentfulBundle{
-    edges{
-      node{
-        id
-        title
-        price
-        image{
-          fixed(width:200, height:120){
-            ...GatsbyContentfulFixed_tracedSVG
-          }
-        }
-      }
-    }
-  }
-}
 `
 
 export default IndexPage
